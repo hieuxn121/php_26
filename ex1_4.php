db43ad0f8c65c7e6f22d208b2b807732cfce30a9
nguyenhieu12012000@gmail.com <nguyenhieu12012000@gmail.com>
2020-12-19 12:57:14 +0700

    Bai tap 1.4 in bang cuu chuong

---
+++ ex1_4.php
@@ -0,0 +1,8 @@
+<?php 
+   for ($i=1; $i <= 10 ; $i++) { 
+   	  for ($j=2; $j <=9 ; $j++) { 
+   	  	echo "<span>&nbsp;&nbsp;&nbsp ".$j." x ".$i." = ".$i*$j;
+   	  }
+   	  echo "<br><br><br>";
+   }
+ ?>
